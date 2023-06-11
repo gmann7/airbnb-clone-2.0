eb6d44c56589472086a1a3240ca209221c629569
Gianluca Mannini <gmannini7@gmail.com>
2023-06-11 20:58:28 +0200

    DB prisma schema

--- app/hooks/useRegisterModal.ts
+++ app/hooks/useRegisterModal.ts
@@ -12,5 +12,4 @@ const useRegisterModal = create<RegisterModalStore>((set) => ({
     onOpen: () => set({ isOpen: true }),
     onClose: () => set({ isOpen: false}),
 }));
-
 export default useRegisterModal;
